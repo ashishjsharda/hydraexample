899aadf2d0de97b7afea84a7c70d789fdd624483
Ashish Sharda <ashishjsharda@gmail.com>
2023-05-23 06:55:30 -0500

    update main

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # my_app.py
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
-@hydra.main(config_name="config")
+@hydra.main(config_path="conf", config_name="config")
 def my_app(cfg : DictConfig) -> None:
-    print(cfg.pretty())
+    print(OmegaConf.to_yaml(cfg))
 
 if __name__ == "__main__":
     my_app()
